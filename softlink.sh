a6574e4e94b59b9ccef57a67ad8dae0458675284
root <root@teamcity.c.theinternetofthing.internal>
2019-07-15 14:47:26 +0700

    debug mode

---
+++ softlink.sh
@@ -0,0 +1,20 @@
+
+cd x64
+ln -fs libssl.so.1.1 			libssl.so
+
+ln -fs libz.so.1.2.11			libz.so.1
+ln -fs libz.so.1.2.11			libz.so
+
+ln -fs libjson-c.so.3.0.1		libjson-c.so.3
+ln -fs libjson-c.so.3.0.1		libjson-c.so
+
+
+ln -fs libglog.so.0.0.0		 libglog.so.0
+
+ln -fs  libffi.so.7.1.0		 libffi.so  
+ln -fs  libmount.so.1.1.0	 libmount.so
+ln -fs  libblkid.so.1.1.0	 libblkid.so
+ln -fs  libuuid.so.1	    	 libuuid.so
+ln -fs  libreadline.so.7.0	 libreadline.so
+ln -fs  libncurses.so.5.9	 libncurses.so
+ln -fs  libncurses.so.5.9	 libncurses.so
